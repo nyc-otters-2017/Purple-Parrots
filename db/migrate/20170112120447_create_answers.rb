676a1d589cc95057476b9de42988cf3de11954ff
Jefferson Faseler & John Talio & Nick Himmel <pair+jefferson-faseler.johntalio.nickhimmel@devbootcamp.com>
2017-01-12 15:31:56 -0500

    Create migration files and add fields

--- db/migrate/20170112120447_create_answers.rb
+++ db/migrate/20170112120447_create_answers.rb
@@ -1,4 +1,11 @@
 class CreateAnswers < ActiveRecord::Migration
   def change
+    create_table :answers do |t|
+      t.integer :user_id
+      t.integer :question_id
+      t.string :answer, null: false
+
+      t.timestamps(null: false)
+    end
   end
 end
