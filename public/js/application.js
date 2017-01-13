02c91d973fe6ed8d884912e8342b1b463e5bd550
Jefferson Faseler & John Talio & Nick Himmel <pair+jefferson-faseler.johntalio.nickhimmel@devbootcamp.com>
2017-01-12 20:32:13 -0500

    Beginning some ajax but not quite ready

--- public/js/application.js
+++ public/js/application.js
@@ -1,3 +1,19 @@
 $(document).ready(function(){
 
+  $(".post-question").submit(function(e) {
+    e.preventDefault();
+
+    var url = $(e.target).attr("action")
+    $.ajax({
+      method: "post",
+      url: url
+    }).success(function(response) {
+
+    }).fail(function(error) {
+
+    });
+  });
+
+
+
 });
