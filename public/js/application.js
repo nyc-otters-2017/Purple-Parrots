a601ae5eff217a83925b7bf2013d3e99ddc16277
John Talio <johntalio@yahoo.com>
2017-01-13 18:29:13 -0500

    Fix ajax for answer route

--- public/js/application.js
+++ public/js/application.js
@@ -1,19 +1,16 @@
 $(document).ready(function(){
 
-  $("#answer-form").submit(function(e) {
+  $("#answer-form").on("submit", function(e) {
     e.preventDefault();
 
     var url = $(e.target).attr("action");
     var data = $(e.target).serialize();
-    debugger
     $.ajax({
-      method: "post",
+      type: "post",
       url: url,
       data: data
     }).success(function(response) {
-      // debugger
-      console.log("hi")
-      // $(".show-question").append(response);
+      $(".answer-container").append(response);
     }).fail(function(error) {
       console.log(error)
     });
