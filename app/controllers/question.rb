3a2317e5230d2448654fc35f6f6d5bf4f25fa494
Jefferson Faseler & John Talio & Nick Himmel <pair+jefferson-faseler.johntalio.nickhimmel@devbootcamp.com>
2017-01-12 20:29:11 -0500

    Working on routes still - trying to post created question

--- app/controllers/question.rb
+++ app/controllers/question.rb
@@ -1,21 +1,31 @@
+get '/questions' do
+  @questions = Question.all
+
+  erb :'/questions/index'
+  # this page should display posted questions
+  # will probably need to change to the home page erb
+end
+
 get '/questions/new' do
+  # may need current_user to persist question once posted
   erb :'/questions/new'
+  # this page should contain the question form
+
+  # once the user submits the question form they should be
+  # directed to a page which contains the posted question with
+  # the form to submit answer and comments
 end
 
 post '/questions' do
-  @question = Question.create( title: params[:title],
-                      question: params[:question] )
+  @question = Question.create( title: params[:title], question: params[:question] )
   if @question.save
     status 200
-    if request.xhr?
-      erb :'/questions/_post_question'
-    else
-      redirect '/questions/new'
-    end
+    redirect "/questions/#{question.id}"
   else
     status 422
+    redirect '/questions/new'
   end
-
+  # this page should post the question
 end
 
 # get '/questions/:id/edit' do
@@ -23,6 +33,12 @@ end
 #
 #   erb :'questions/edit'
 # end
+
+get '/questions/:id' do
+  @question = Question.find(params[:id])
+
+  erb :'/questions/show'
+end
 
 delete '/questions/:id' do
   # id of question must match the id of the user deleting
