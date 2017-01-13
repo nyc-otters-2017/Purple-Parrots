a6f610b67781a3a3517c205c816787afc42a932a
John Talio <johntalio@yahoo.com>
2017-01-13 15:06:04 -0500

    beginning of ajax for answers - need routes

--- public/js/application.js
+++ public/js/application.js
@@ -3,11 +3,15 @@ $(document).ready(function(){
   $("#answer-form").submit(function(e) {
     e.preventDefault();
 
-    var url = $(e.target).attr("action")
+    var url = $(e.target).attr("action");
+    var data = $(e.target).serialize();
+    debugger
     $.ajax({
       method: "post",
-      url: url
+      url: url,
+      data: data
     }).success(function(response) {
+      // debugger
       console.log("hi")
       // $(".show-question").append(response);
     }).fail(function(error) {
